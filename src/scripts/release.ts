50bdb6e9649121a038ad88abe4b376ac20b721fd
tkitsunai <kitsunai.project@gmail.com>
2025-01-11 10:09:46 +0900

    initial commit

---
+++ src/scripts/release.ts
@@ -0,0 +1,33 @@
+import * as fs from "fs";
+import * as path from "path";
+import * as archiver from "archiver";
+import { execSync } from "child_process";
+
+const { version } = JSON.parse(fs.readFileSync("package.json", "utf-8"));
+
+const outputDir = "dist";
+const releaseDir = "release";
+const filterFileName = `v${version}_kits_item_filter.filter`;
+
+// create release directory
+if (!fs.existsSync(releaseDir)) {
+  fs.mkdirSync(releaseDir, { recursive: true });
+}
+
+const filterFilePath = path.join(outputDir, filterFileName);
+const baseFilterFilePath = "assets/kits-item-filter.filter";
+
+fs.copyFileSync(baseFilterFilePath, filterFilePath);
+
+// zip configuration
+const zipFilePath = `release/v${version}_kits_item_filter.zip`;
+const output = fs.createWriteStream(zipFilePath);
+const archive = archiver("zip", { zlib: { level: 9 } });
+
+output.on("close", () => {
+  console.log(`ZIP Created: ${zipFilePath}`);
+});
+
+archive.pipe(output);
+archive.file(filterFilePath, { name: filterFileName });
+archive.finalize();
